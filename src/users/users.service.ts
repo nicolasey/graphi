52136f910313844bbc035c64520484c29972bebc
nicolasey <nicolas.sey@gmail.com>
2020-07-11 20:38:43 +0200

    add find methods to user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,17 +3,27 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel('User') private userModel: Model<User>) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const createdCat = new this.userModel(createUserDto);
-    return createdCat.save();
+    createUserDto.password = await bcrypt.hash(createUserDto.password, 10);
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
   }
 
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec();
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.userModel.findOne({ email }).exec();
+  }
+
+  async find(id: string): Promise<User> {
+    return this.userModel.findOne({ _id: id }).exec();
   }
 }
