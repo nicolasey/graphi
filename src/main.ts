c2e0e53e3ae3d9313cf2c425e59768e96d36a883
Nicolas Seynaeve <nicolas.sey@gmail.com>
2020-10-17 16:22:50 +0200

    add basic security

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestEnlighten } from 'nestjs-enlighten';
+import * as helmet from "helmet";
+import * as rateLimit from "express-rate-limit";
+import "dotenv/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: process.env.RATE_LIMIT_MS,
+      max: process.env.RATE_LIMIT_NB,
+    })
+  );
   app.useGlobalFilters(new NestEnlighten({ theme: 'theme-dark' }));
   await app.listen(3000);
 }
